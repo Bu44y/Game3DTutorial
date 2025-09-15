a058b1f7dd4b017128040b9f4e0c4bb08a5a2484
Bu44y <chaiwatnasa@yahoo.com>
2025-09-15 14:04:22 +0700

    Camera

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Transform target;
+    public Vector3 offset;
+    public Space offsetSpace = Space.Self;
+    private bool lookAt = true;
+
+    private void LateUpdate()
+    {
+        if (target == null)
+        {
+            return;
+        }
+        if (offsetSpace == Space.Self)
+        {
+            transform.position = target.TransformPoint(offset);
+        }
+        else
+        {
+            transform.position = target.position + offset;
+        }
+
+        if (lookAt)
+        {
+            transform.LookAt(target);
+        }
+        else
+        {
+            transform.position = target.position;
+        }
+    }
+}
